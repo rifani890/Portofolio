e86461938010c69ae9d62b4048958485d076393b
rifani890 <arrifani0@gmail.com>
2022-06-29 21:03:03 +0700

    update email

--- dist/js/script.js
+++ dist/js/script.js
@@ -54,25 +54,25 @@ if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.match
 }
 
 // // Contact
-// const contactForm = document.getElementById("contact-form");
-// const loader = document.querySelector(".loader");
+const contactForm = document.getElementById("contact-form");
+const loader = document.querySelector(".loader");
 
-// loader.style.display = "none";
+loader.style.display = "none";
 
-// contactForm.addEventListener("submit", function (e) {
-//   e.preventDefault();
-//   loader.style.display = "block";
-//   const url = e.target.action;
-//   const formData = new FormData(contactForm);
+contactForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+  loader.style.display = "block";
+  const url = e.target.action;
+  const formData = new FormData(contactForm);
 
-//   fetch(url, {
-//     method: "POST",
-//     body: formData,
-//     mode: "no-cors",
-//   })
-//     .then(() => {
-//       loader.style.display = "none";
-//       window.location.href = "/thank.html";
-//     })
-//     .catch((e) => alert("Error occured"));
-// });
+  fetch(url, {
+    method: "POST",
+    body: formData,
+    mode: "no-cors",
+  })
+    .then(() => {
+      loader.style.display = "none";
+      window.location.href = "thankyou.html";
+    })
+    .catch((e) => alert("Error occured"));
+});
